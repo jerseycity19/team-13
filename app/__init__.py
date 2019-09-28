c177fc67f279a5a6190a919ed92aad823e43fbee
Jeffrey Lin <jeffrey@icurse.nl>
2019-09-28 00:23:35 -0400

    __init__.py: force using firebase.json

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
-from firebase_admin import credentials, firestore, initialize_app
+from firebase_admin import credentials, firestore, initialize_app, storage
 from flask import Flask
 
 app = Flask(__name__)
 
 # Use the application default credentials
-cred = credentials.ApplicationDefault()
+cred = credentials.Certificate('firebase.json')
 initialize_app(cred, {
-  'projectId': "code-for-good-nj-13",
+    'projectId': "code-for-good-nj-13",
+    'storageBucket': 'code-for-good-nj-13.appspot.com'
 })
 
 db = firestore.client()
+bucket = storage.bucket()
 
 from app import routes
