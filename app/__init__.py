3e7498d2a54a39945e0679effe2f7bfbca501aad
Jeffrey Lin <jeffrey@icurse.nl>
2019-09-27 21:47:00 -0400

    Move firebase auth to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,14 @@
+from firebase_admin import credentials, firestore, initialize_app
 from flask import Flask
+
 app = Flask(__name__)
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+initialize_app(cred, {
+  'projectId': "code-for-good-nj-13",
+})
+
+db = firestore.client()
+
 from app import routes
