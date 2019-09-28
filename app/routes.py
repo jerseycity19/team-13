d577275ed7fa398dd9c604d86b31b768c96619a7
David <davidyang979@gmail.com>
2019-09-27 23:33:46 -0400

    Updated routes.py to add countries

--- app/routes.py
+++ app/routes.py
@@ -6,10 +6,17 @@ import json
 @app.route('/index', methods = ['GET'])
 def index():
     UserID = ['Scholar', 'Student', 'Administrator', 'Non-University']
-    return render_template('test.html', UserID=UserID)
+    # return render_template('test.html', UserID=UserID)
+    return "Hello World"
 
 @app.route('/languages')
 def languages():
     language_file = open("app/data/languages.json")
     languages = json.load(language_file)
     return render_template('languages.html', languages=languages)
+
+@app.route('/countries')
+def countries():
+    country_file = open("app/data/country-by-continent.json")
+    countries = json.load(country_file)
+    return render_template('countries.html', countries=countries)
